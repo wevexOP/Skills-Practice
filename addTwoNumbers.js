e3939fd850454e051546123f7f8f64e882cda10b
Dominic <dompaulazzo@gmail.com>
2023-03-14 13:42:25 -0700

    finalizing addTwoNumbers.js problem

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -13,24 +13,39 @@
  
 /*This is problem 2 from leetcode.com  */
 
-const l1 = [2,4,3]; 
-const l2 = [5,6,4];
 
 var addTwoNumbers = function(l1, l2) {
+    let carry = 0;
+    let tail = null
+    let head = null; 
     while (l1 || l2){
         let num1 = l1?.val ?? 0;
         let num2 = l2?.val ?? 0;
 
-        console.log(num1, num2);
-        const sum = num1 + num2;
-        console.log(sum);
+        const sum = num1 + num2 + carry
+        carry = Math.floor(sum / 10);
+        sum == (carry * 10);
 
-       
+        if (!tail) {
+            tail = new ListNode(sum, null);
+            head = tail;
+        } else {
+            head.next = new ListNode(sum, null);
+            head = head.next;
+        }
+
         if (l1) {
             l1 = l1.next;
         }
         if (l2) {
             l2 = l2.next;
         }
+        
+        if (carry > 0) {
+            head.next = new ListNode(sum, null);
+            head = head.next;
+        }
+
+        return tail;
     }
 };
