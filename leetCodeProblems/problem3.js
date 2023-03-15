27e4b883867a2515c5312afa3b82678edde55892
Dominic <dompaulazzo@gmail.com>
2023-03-15 09:36:37 -0700

    progressing through the problem 3

--- leetCodeProblems/problem3.js
+++ leetCodeProblems/problem3.js
@@ -3,5 +3,10 @@
  * @return {number}
  */
  var lengthOfLongestSubstring = function(s) {
-    
+    for (let i=0;i<s.length;i++) {
+        for (let j=i+1;j<=s.length;j++) {
+            const substring = s.substring(i, j);
+            console.log(substring);
+        }
+    }
 };
