641a944539403f626ac91eedad9a8aa38128c548
Dominic <dompaulazzo@gmail.com>
2023-03-15 09:44:39 -0700

    additions to problem 3

--- leetCodeProblems/problem3.js
+++ leetCodeProblems/problem3.js
@@ -7,6 +7,18 @@
         for (let j=i+1;j<=s.length;j++) {
             const substring = s.substring(i, j);
             console.log(substring);
+
+            const charsFound = {};
+            let isOk = true;
+            for (let k=0;k<substring.length;k++) {
+                const character = substring[k];
+                if (!charsFound[char]){
+                    charsFound[char] = true;
+                } else {
+                    isOK = false;
+                    break;
+                }
+            }
         }
     }
 };
