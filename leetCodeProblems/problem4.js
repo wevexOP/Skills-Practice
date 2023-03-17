cb88667bb9f30d1192d89d2b59847cb8b92cd384
Dominic <dompaulazzo@gmail.com>
2023-03-17 11:53:47 -0700

    solving problem 4 on leet code

---
+++ leetCodeProblems/problem4.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+ var findMedianSortedArrays = function(nums1, nums2) {
+    const merged = [...nums1, ...num2].sort((a,b)=>a-b);
+    const mid = Math.floor(merged.length/2);
+
+    return merged.length % 2 === 0?
+        (merged[mid-1] + merged[mid])/2 : merged[mid];
+};
