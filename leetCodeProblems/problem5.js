505e860792075dada2c5116fa23e57972c196c1c
Dominic <dompaulazzo@gmail.com>
2023-03-20 10:16:12 -0700

    initialize by setting result to equal an empty string and result length to 0

--- leetCodeProblems/problem5.js
+++ leetCodeProblems/problem5.js
@@ -5,5 +5,7 @@
  * @return {string}
  */
  var longestPalindrome = function(s) {
-    
+    // initialize by setting result to equal an empty string and result length to 0
+    res = ""
+    resLength = 0
 };
