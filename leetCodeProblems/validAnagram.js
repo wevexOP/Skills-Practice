9b20858ab4bd44c8c589a4427c2d64c07893be9b
Dominic <dompaulazzo@gmail.com>
2023-03-21 10:58:42 -0700

    adding valid anagram solved from leet code to the repo

--- leetCodeProblems/validAnagram.js
+++ leetCodeProblems/validAnagram.js
@@ -4,5 +4,38 @@
  * @return {boolean}
  */
  var isAnagram = function(s, t) {
-    
+    // if s.length !== t.length, return false
+    if (s.length !== t.length){
+        return false;
+    }
+    // create a s letter object
+    let sLetter = {};
+   // for let char of s     
+    for (let char of s){
+        // if sLetter[char] exists, increment
+        if (sLetter[char]){
+            sLetter[char]++;
+        }
+        // else, set sLetter[char] to 1
+        else {
+            sLetter[char] = 1;
+        }
+    }
+    // for let char of t and return false if sLetter[char] doesn't exist 
+    for (let char of t){
+        if (!sLetter[char]){
+            return false;
+        }
+        // if slLetter[char]
+        if (sLetter[char]){
+            sLetter[char]--;
+        }
+        // if sLetter[char] === 0, delete sLetter[char]
+        if (sLetter[char] === 0){   
+            delete sLetter[char];
+        }
+    }
+
+    // return object.keys(sLetter).length === 0
+    return Object.keys(sLetter).length === 0;
 };
