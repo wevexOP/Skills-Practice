b3c5b6dd3c11bc545bf2a96c6b732b59f5354fed
Dominic <dompaulazzo@gmail.com>
2023-03-21 10:31:34 -0700

    solved contains duplicate with added notes

--- NeetCode.io/containsDuplicate.js
+++ NeetCode.io/containsDuplicate.js
@@ -1,14 +1,23 @@
 /**
- * Hash Set
- * Time O(N) | Space O(N)
- * https://leetcode.com/problems/contains-duplicate/
  * @param {number[]} nums
  * @return {boolean}
  */
- var containsDuplicate = (nums) => {
-    const numsSet = new Set(nums);/* Time O(N) | Space O(N) */
-    const isEqual = numsSet.size === nums.length;
-
-    return !isEqual;
+ var containsDuplicate = function(nums) {
+    let hTable = {};
+    // create for loop to iterate through the array
+    for (let i = 0; i < nums.length; i++) {
+        // if the hash table has the value of the array, return true
+        if (hTable[nums[i]]) {
+            return true;
+        }
+        // else, set the value of the array to the hash table
+        else {
+            hTable[nums[i]] = 1;
+        }
+    }
+    return false;
 };
+
+let test1 = [1,2,3,1];
+let test2 = [1,2,3,4];
 
