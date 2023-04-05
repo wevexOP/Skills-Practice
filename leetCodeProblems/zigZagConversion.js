5f6bab69b3ed92eaee02fee3907e919c1ca83f17
Dominic <dompaulazzo@gmail.com>
2023-04-05 14:02:20 -0700

    working through zigZagConversion.js

--- leetCodeProblems/zigZagConversion.js
+++ leetCodeProblems/zigZagConversion.js
@@ -4,5 +4,27 @@
  * @return {string}
  */
  var convert = function(s, numRows) {
-    
+    // set up and open arr
+    const result = [];
+
+    // if numRows is less than 2, return s
+    if (numRows < 2) {
+        return s;
+    }
+
+    // create a for loop to iterate through numRows
+    for (let i = 0; i < numRows; i++) {
+        let j = i;
+        //while j is less than s.length
+        while (j < s.length) {
+            // push s[j] into result[i]
+            result[i].push(s[j]);
+            // if i is not 0 and i is not numRows - 1
+            if (i !== 0 && i !== numRows - 1) {
+                //const k = j + (numRows - i - 1) * 2;
+                // push s[k] into result[i]
+                // push s[j + (numRows - i - 1) * 2] into result[i]
+                result[i].push(s[j + (numRows - i - 1) * 2]);
+            }
+    }
 };
