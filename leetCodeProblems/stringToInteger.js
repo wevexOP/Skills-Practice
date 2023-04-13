eda50f632e0d44155321a98eeb61354c66386602
Dominic <dompaulazzo@gmail.com>
2023-04-13 10:30:35 -0700

    completed code for stringToInteger

--- leetCodeProblems/stringToInteger.js
+++ leetCodeProblems/stringToInteger.js
@@ -4,8 +4,43 @@
  */
  var myAtoi = function(s) {
     let index = 0;
-    let sign = 1;
-    let total = 0;
-    // 1. Empty string
-    if (s.length === 0) return 0;
+    let isNeg = false;
+    let res = 0;
+    
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === ' ') {
+            index++;
+        } else {
+            break;
+        }
+    }
+
+    if(s[index] === '-' || s[index] === '+') {
+        isNeg = s[index] === '-';
+        index++;
+    }
+
+    for(let i = index; i < s.length; i++) {
+        
+        let num = s.charCodeAt(i) - 48;
+
+        if(num < 0 || num > 9) break;
+
+        res *= 10;
+        res += num;
+
+    }
+
+    if(isNeg) {
+        res = -res;
+    }
+
+    let min = -(2 ** 31);
+    let max = 2 ** 31 - 1;
+
+    let minima = Math.max(res, min);
+    
+    return Math.max(minima, min);
+
+
 };
