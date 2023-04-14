a54c7dbe2f6f9a2f567fc9421b0ebae88d4e389c
Dominic <dompaulazzo@gmail.com>
2023-04-14 13:36:21 -0700

    updated code with github copilot, incorrect

--- leetCodeProblems/palindromeNumber.js
+++ leetCodeProblems/palindromeNumber.js
@@ -3,5 +3,24 @@
  * @return {boolean}
  */
  var isPalindrome = function(x) {
+    // convert to string
+    let str = x.toString();
+    // split string into array
+    let arr = str.split('');
+    // reverse array
+    let revArr = arr.reverse();
+    // join array into string
+    let revStr = revArr.join('');
+    // convert string to number
+    let revNum = parseInt(revStr);
+    // check if number is negative
+    if (x < 0) {
+        revNum = revNum * -1;
+    }   
+    // check if number is a palindrome
+    if (x === revNum) {
+        return true;
+    }
+    return false;
     
 };
